00d8c53a73a95612375953bd6eb2c730d4e5b9d3
mono <you@example.com>
2019-11-25 00:58:47 +0300

    Receiving token

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import "./App.module.sass";
 import { Token } from "./components/Token";
+import Container from "@material-ui/core/Container";
+import { Groups } from "./components/Groups";
 
 const App: React.FC = () => {
+  const [groups, setGroups] = useState<{ [key: string]: any } | null>(null);
+  const [token, changeToken] = useState<string | null>(null);
+
+  const getToken = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const url = new URL(e.target.value.replace("#", "?"));
+    changeToken(url.searchParams.get("access_token"));
+  };
+
   return (
-    <div>
-      <Token />
-    </div>
+    <Container maxWidth="md">
+      <Token getToken={getToken} />
+      <Groups groups={groups} />
+    </Container>
   );
 };
 
--- src/components/Token/Token.tsx
+++ src/components/Token/Token.tsx
@@ -2,11 +2,28 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 import styles from "./Token.module.sass";
 
-const Token: React.FC = () => {
+type PropsType = {
+  getToken: (x: any) => any;
+};
+
+const Token: React.FC<PropsType> = props => {
+  const { getToken } = props;
   return (
     <div className={styles.container}>
-      <a className={styles.label}>Получить токен</a>
-      <TextField id="input-token" label="Токен" variant="outlined"></TextField>
+      <a
+        href={
+          "https://oauth.vk.com/authorize?client_id=7218002&redirect_uri=https://oauth.vk.com/blank.html&display=popup&scope=wall,offline,groups&response_type=token&v=5.103"
+        }
+        className={styles.label}
+      >
+        Получить токен
+      </a>
+      <TextField
+        id="input-token"
+        label="Токен"
+        variant="outlined"
+        onChange={getToken}
+      ></TextField>
     </div>
   );
 };
